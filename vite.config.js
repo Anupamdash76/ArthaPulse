b644b7d9dd7851e949481ab338fb5a809f7946bf
Anupam Dash <anupamdash2004@gmail.com>
2025-10-16 01:30:01 +0530

    Fix case-sensitive imports for Netlify

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ⚡ important for Netlify / relative paths
+  base: './', // ⚡ important for relative paths on Netlify
 });
